df7079d6e053aa36e106eff7e91e3e7e3cf29c5c
EjTheGreat0 <143009232+EjTheGreat0@users.noreply.github.com>
2024-12-19 21:23:14 +0800

    Add files via upload

---
+++ MyConnectorDataBase.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2024 at 02:19 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myconnector`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `myconnector`
+--
+
+CREATE TABLE `myconnector` (
+  `StudentID` int(11) NOT NULL,
+  `Firstname` varchar(45) NOT NULL,
+  `Surname` varchar(45) NOT NULL,
+  `Address` varchar(45) NOT NULL,
+  `Post Code` varchar(45) NOT NULL,
+  `Telephone` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `myconnector`
+--
+ALTER TABLE `myconnector`
+  ADD PRIMARY KEY (`StudentID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `myconnector`
+--
+ALTER TABLE `myconnector`
+  MODIFY `StudentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
